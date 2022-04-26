1d387c81185fe62e4bae8a94443872cab31d9630
Jonathan <jonan270@student.liu.se>
2022-04-26 20:53:47 +0200

    Style updates and simple body added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
+      <Content/>
     </div>
   );
 }
