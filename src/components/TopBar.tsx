6aa27977edb5018f034c9d52d1dc613e40e05cd9
Jonathan <jonan270@student.liu.se>
2022-04-26 19:50:15 +0200

    Top title refinement

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,10 +1,12 @@
-
-
+import profile from "../images/profile.jpg"
+//<img src={profile} id='profileImg'/>
 const TopBar: React.FC<{}> = () => {
     return(
-        <div>
-        <h1>Jonathan Andersson</h1>
-        <p>Lorum ipsum sit amet</p>
+        <div id = 'topBar'>
+            <div id='titleText'>
+                <h1>Jonathan Andersson</h1>
+                <p>4th year M.Sc Student in Media Technology</p>
+            </div>
         </div>
     )
 }
