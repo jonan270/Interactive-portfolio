97e3192de537f789d05945479fbb4318a808e517
Jonathan <jonan270@student.liu.se>
2022-04-26 18:49:04 +0200

    Basic custom styling

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,12 @@
+
+
+const TopBar: React.FC<{}> = () => {
+    return(
+        <div>
+        <h1>Jonathan Andersson</h1>
+        <p>Lorum ipsum sit amet</p>
+        </div>
+    )
+}
+
+export default TopBar;
