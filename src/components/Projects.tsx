653b02a760221f11a6e4f5c0e76d6fb8717b7ff2
Jonathan <jonan270@student.liu.se>
2022-04-27 18:38:33 +0200

    Added content sub-components

---
+++ src/components/Projects.tsx
@@ -0,0 +1,18 @@
+const Projects: React.FC<{}> = () => {
+    return(
+        <div id = 'aboutMe'>
+            <h1>Project showroom</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus nisl
+                sed lacus gravida vehicula. Nunc nec erat ut leo eleifend accumsan. Aenean
+                eget nisi ac dui imperdiet cursus. Sed sit amet mattis ex.
+                
+                Aliquam erat volutpat. Nullam finibus vel augue nec placerat. Aliquam vitae
+                risus ac elit semper imperdiet nec vel augue. Mauris et suscipit ipsum.
+                Donec pellentesque in sapien eu tincidunt. Nam ac eros non libero tempus blandit.
+                Praesent non felis ac lorem consequat vulputate.
+                Aenean at tristique justo, et efficitur libero. </p>
+        </div>
+    )
+}
+
+export default Projects;
