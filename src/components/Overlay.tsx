26f9986b9f8827fe29346e71299cbd7f72536ff2
jonan270 <57438307+jonan270@users.noreply.github.com>
2022-07-22 18:06:16 +0200

    Started implementing project overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/overlay.scss"
 
 interface Props {
     overlay: Overlay
@@ -7,7 +8,20 @@ interface Props {
 const Overlay: React.FC<Props> = ({overlay}) => {
     return(
         <div>
-            {overlay.showOverlay ? <h1>Overlay</h1> : null}
+            {overlay.showOverlay ? 
+            <div className="background" onClick={() => overlay.setOverlay(false)}>
+                <div className="foreground" onClick={() => {}}>
+                    <h1>Test title!</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur,
+                        adipisicing elit. Fuga, vel magnam? Quidem
+                        impedit doloribus nemo quam, eum odit
+                        nesciunt qui molestiae tempora repellat.
+                        Incidunt quisquam reiciendis corporis minus
+                        praesentium id!</p>
+                </div>
+            </div>
+            : null}
         </div>
     )
 }
