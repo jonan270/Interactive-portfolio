3363eae45e1e33a98db4edaebf40522842ab4514
jonan270 <57438307+jonan270@users.noreply.github.com>
2022-08-04 15:29:55 +0200

    Added information legend

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,11 +1,33 @@
 import profile from "../images/profile.jpg"
 import "../styles/images.scss"
+import "../styles/about.scss"
+import EmailIcon from '@mui/icons-material/Email';
+import { PhoneAndroid as Phone } from "@mui/icons-material";
+import SchoolIcon from "@mui/icons-material/School";
 
 const AboutMe: React.FC<{}> = () => {
     return(
         <div id = "about" className="chapter">
             <img src={profile} alt="profile_image" id='profileImg'/>
             <h1>About me</h1>
+            <div className="iconTextRow">
+                <Phone color="primary"/>
+                <p className="iconText">+46703107498</p>
+            </div>
+            <div className="iconTextRow">
+                <EmailIcon color="primary"/>
+                <p className="iconText">cg.jonathan.andersson@gmail.com</p>
+            </div>
+            <div className="iconTextRow">
+                <SchoolIcon color="primary"/>
+                <a
+                className="iconText"
+                href="https://studieinfo.liu.se/en/program/6CMEN"
+                target="_blank" rel="noreferrer"
+                >
+                    5th year M.Sc Student in Media Technology
+                </a>
+            </div>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus nisl
                 sed lacus gravida vehicula. Nunc nec erat ut leo eleifend accumsan. Aenean
